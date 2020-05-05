bc5634a52adea86de05e8cf18fb13d9fca6b3225
Alexander Færøy <ahf@0x90.dk>
2020-05-05 22:10:51 +0000

    Update offlineimap.
    Use new password manager wrapper, which handles locking properly.

--- mail/.mail/scripts/offlineimap.py
+++ mail/.mail/scripts/offlineimap.py
@@ -4,22 +4,31 @@
 # Alexander Færøy <ahf@0x90.dk>
 # PGP: 0x61A208E16E7CB435
 #
-# This script is used to read passphrases from pass(1) and feed them to
-# Offline IMAP.
-#
-# A main-function is provided for debugging purposes.
+# This script is used to read passphrases from my modified Qubes aware pass(1)
+# and feed them to Offline IMAP.
 #
 
 import sys
 import subprocess
+import threading
+
+class PasswordManager:
+    def __init__(self):
+        self._lock = threading.Lock()
+
+    def get_password(self, account):
+        password = None
+
+        self._lock.acquire()
+        try:
+            output = subprocess.check_output("qubes-pass %s" % account, shell=True)
+            password = output.splitlines()[0].decode('utf-8')
+        finally:
+            self._lock.release()
+
+        return password
+
+_password_manager = PasswordManager()
 
 def get_password(account):
-    output = subprocess.check_output("pass %s" % account, shell=True)
-    return output.splitlines()[0].decode('utf-8')
-
-def main(argv):
-    if len(argv) > 1:
-        print("Password: '%s'", get_password(argv[1]))
-
-if __name__ == '__main__':
-    main(sys.argv)
+    return _password_manager.get_password(account)
