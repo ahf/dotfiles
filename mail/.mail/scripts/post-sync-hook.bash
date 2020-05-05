958c7320be43fe3c077f6035cb8933ffb3eb19bf
Alexander Færøy <ahf@0x90.dk>
2020-05-05 22:17:19 +0000

    Handle notmuch filters via files instead of scripts.

--- mail/.mail/scripts/post-sync-hook.bash
+++ mail/.mail/scripts/post-sync-hook.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ -x ~/.mail/scripts/notmuch-post-sync-hook.bash ]] ; then
-    ~/.mail/scripts/notmuch-post-sync-hook.bash
+    ~/.mail/scripts/notmuch-post-sync-hook.bash "$@"
 fi
